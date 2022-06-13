9744a3e2f8b20d46ab84162617731d51a0fe6c68
a421849168 <421849168@qq.com>
2022-06-13 11:23:48 +0800

    添加

---
+++ src/Seam01/day01.java
@@ -0,0 +1,15 @@
+package Seam01;
+
+public class day01 {
+//    public static void main(String[] args) {
+//        int a = 54;
+//                int b =98;
+//                        int c =a<b? a : b;
+//    System.out.println(c);
+//    }
+
+
+    public static void main(String[] args) {
+        System.out.println("hello word !");
+    }
+}
